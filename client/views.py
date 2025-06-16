e37c37a9e99bbb9a744f7ce03f05e79613205c4f
Nidvaleri <valeriyanidelkinav@gmail.com>
2025-06-16 17:09:13 +0600

    Update views.py

--- client/views.py
+++ client/views.py
@@ -2,17 +2,14 @@ from rest_framework import generics
 from .models import Client, CartItem, Order
 from .serializers import ClientSerializer, CartItemSerializer, OrderSerializer
 
-# Клиенты: можно создать нового клиента (оформление заказа) и смотреть список (для админки)
 class ClientListCreateAPIView(generics.ListCreateAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-
-# Детали клиента
+    
 class ClientRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
-# Корзина: показываем, добавляем, обновляем, удаляем товары корзины для конкретного клиента
 class CartItemListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = CartItemSerializer
 
@@ -28,7 +25,6 @@ class CartItemRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
 
-# Заказы: создание и просмотр заказов (для клиента и админа)
 class OrderListCreateAPIView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
